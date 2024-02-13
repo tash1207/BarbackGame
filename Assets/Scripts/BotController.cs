0e885b4a9e2d3645adc0f215bd75c778743009f9
Tasha Hankewych <thankewych@gmail.com>
2024-02-13 00:04:03 -0600

    Create simple 2D scene with moveable character, objects, and colliders

---
+++ Assets/Scripts/BotController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x = position.x + 3.2f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.2f * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+}
