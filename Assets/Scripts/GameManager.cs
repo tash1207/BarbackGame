b7b887604ce1d17f084a79a7cc53be555b77eaa0
Tasha Hankewych <thankewych@gmail.com>
2024-02-19 15:27:06 -0600

    Set the newly spawned beers and trays as children of the table objects so the correct sorting order is applied

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -45,9 +45,8 @@ public class GameManager : MonoBehaviour
         float randomX = Random.Range(-0.7f, 0.7f);
         float randomY = Random.Range(-0.7f, 0.7f);
         Vector2 beerPosition = new Vector2(table.transform.position.x + randomX, table.transform.position.y + randomY);
-        Instantiate(beerPrefab, beerPosition, Quaternion.identity);
-        // TODO: Set beer as a child of the table so the sorting group is applied.
-        // beerPrefab.transform.parent = table.transform;
+        GameObject newBeer = Instantiate(beerPrefab, beerPosition, Quaternion.identity) as GameObject;
+        newBeer.transform.parent = table.transform;
         // TODO: Adjust time depending on how many removables are on screen.
         beerTimer = beerChangeTime;
     }
@@ -59,8 +58,8 @@ public class GameManager : MonoBehaviour
         float randomX = Random.Range(-0.4f, 0.4f);
         float randomY = Random.Range(-0.5f, 0.7f);
         Vector2 trayPosition = new Vector2(table.transform.position.x + randomX, table.transform.position.y + randomY);
-        Instantiate(Random.Range(0, 2) < 1 ? blueTrayPrefab : redTrayPrefab, trayPosition, Quaternion.identity);
-        // TODO: Set tray as a child of the table so the sorting group is applied.
+        GameObject newTray = Instantiate(Random.Range(0, 2) < 1 ? blueTrayPrefab : redTrayPrefab, trayPosition, Quaternion.identity) as GameObject;
+        newTray.transform.parent = table.transform;
         trayTimer = trayChangeTime;
     }
 }
